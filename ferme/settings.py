b6d30aa3a4c3fbc3aeadbad9049527a4081bc83d
Drew <pedrewscm@gmail.com>
2019-04-11 18:25:40 -0400

    added product_list view and route path

--- ferme/settings.py
+++ ferme/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'ferme.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+             BASE_DIR
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
