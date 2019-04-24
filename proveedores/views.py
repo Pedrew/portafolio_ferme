674c483e458d4a6f11b226e498429ad953ed3fa3
Gitvj <vajb144@gmail.com>
2019-04-24 20:59:05 -0300

    Error Template Does not Exist

--- proveedores/views.py
+++ proveedores/views.py
@@ -20,3 +20,21 @@ def proveedores_insert(request):
 	Proveedor.createProveedor(nombre, rut, mail, telefono)
 	response = redirect('proveedores_list')
 	return response 
+
+def proveedores_get(request):
+	get = request.GET
+	proveedor_id = get['id']
+	response = Proveedor.getProveedor(proveedor_id)
+	return render(request, 'proveedores/proveedores_update.html',{'proveedor':response})
+
+def proveedores_update(request):
+	post = request.POST
+	proveedor_id = post['proveedor_id']
+	nombre = post['nombre']
+	rut = post['rut']
+	mail = post['mail']
+	telefono = post['telefono']
+	Proveedor.updateProveedor(proveedor_id, nombre, rut, mail, telefono)
+	response = redirect('proveedores_list')
+	return response
+
