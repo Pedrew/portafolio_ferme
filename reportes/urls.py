de75cff8520b524e6c5f71f7392d0e98aa69dc1b
drew <pedrewscm@gmail.com>
2019-07-06 16:18:22 -0400

    god protect us

--- reportes/urls.py
+++ reportes/urls.py
@@ -3,4 +3,5 @@ from reportes.views import *
 
 urlpatterns = [
 	url(r'^boletas_anno', boletas_anno, name='boletas_anno'),
+    url(r'^boletas_mes', boletas_mes, name='boletas_mes'),
 ]
--- reportes/views.py
+++ reportes/views.py
@@ -3,8 +3,15 @@ from .models import Reportes
 from django.shortcuts import redirect
 
 # Create your views here.
-def boletas_anno(request, ano):
+def boletas_anno(request):
     post = request.POST
     anno = post['anno']
     boletas_ano = Reportes.boletasAnno(anno)
     return render(request, 'reportes/boletas_anno.html',{ 'boletas' : boletas_ano})
+
+def boletas_mes(request):
+    post = request.POST
+    mes = post['mes']
+    ano = post['ano']
+    boletas_mes = Reportes.boletasMes(mes, ano)
+    return render(request, 'reportes/boletas_mes.html',{ 'boletas' : boletas_mes})
